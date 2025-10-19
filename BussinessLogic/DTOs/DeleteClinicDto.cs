0140fae708086add3c5ee17ce8f7cbab7b4c54b7
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-10-19 22:38:01 +0300

    Add files via upload

---
+++ BussinessLogic/DTOs/DeleteClinicDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic.DTOs
+{
+    public class DeleteClinicDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
