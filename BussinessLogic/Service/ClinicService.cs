e5ca9fcb2875c2821e2e21a77166315329ba05d9
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-10-19 22:38:32 +0300

    Add files via upload

---
+++ BussinessLogic/Service/ClinicService.cs
@@ -0,0 +1,76 @@
+﻿using BussinessLogic.DTOs;
+using DataAccess.Entities;
+using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic.Service
+{
+    public class ClinicService : IClinicService
+    {
+        private readonly IClinicRepository _clinic;
+
+        public ClinicService(IClinicRepository clinic)
+        {
+            _clinic = clinic;
+        }
+
+        public async Task<IEnumerable<GetClinicDto>> GetAllClinics()
+        {
+            var ClinicFromRepo = await _clinic.GetAllClinics();
+            var Clinicdto = ClinicFromRepo.Select(cat => new GetClinicDto()
+            {
+                Name = cat.Name,
+                Phone = cat.Phone,
+                Address = cat.Address,
+            });
+            return Clinicdto;
+        }
+
+
+        public async Task<GetClinicDto> GetClinicOnlyByName(string name)
+        {
+            var Clinicfromrepo = await _clinic.GetClinicByName(name);
+            var ClinicDto = new GetClinicDto()
+            {
+                Name = Clinicfromrepo.Name,
+                Phone = Clinicfromrepo.Phone,
+                Address = Clinicfromrepo.Address
+            };
+            return ClinicDto;
+        }
+
+
+        public async Task AddClinic(AddClinicDto dto)
+        {
+            var Clinictorepo = new Clinic()
+            {
+                Name = dto.Name,
+                Phone = dto.Phone,
+                Address= dto.Address
+            };
+
+            await _clinic.AddClinic(Clinictorepo);
+        }
+
+
+        public async Task EditClinic(AddClinicDto dto)
+        {
+            var Clinictorepo = new Clinic()
+            {
+                Name = dto.Name,
+                Phone = dto.Phone,
+                Address = dto.Address,
+            };
+            await _clinic.EditClinic(Clinictorepo);
+        }
+
+        public async Task DeleteClinic(DeleteClinicDto dto)
+        {
+            await _clinic.DeleteClinic(dto.Name, dto.Address); 
+        }
+    }
+}
---
+++ BussinessLogic/Service/IClinicService.cs
@@ -0,0 +1,19 @@
+﻿using BussinessLogic.DTOs;
+using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic.Service
+{
+    public interface IClinicService
+    {
+        Task<IEnumerable<GetClinicDto>> GetAllClinics();
+        Task<GetClinicDto> GetClinicOnlyByName(string name);
+        Task AddClinic(AddClinicDto dto);
+        Task EditClinic(AddClinicDto dto);
+        Task DeleteClinic(DeleteClinicDto dto);
+    }
+}
