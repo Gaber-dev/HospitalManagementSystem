d84c0475b973a989057273d8686ca53a31f746ef
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-10-19 22:37:12 +0300

    Add files via upload

---
+++ DataAccess/Repository/IClinicRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public interface IClinicRepository
+    {
+        Task<IEnumerable<Clinic>> GetAllClinics();
+        Task<Clinic> GetClinicByName(string name);
+        Task AddClinic(Clinic clinic);
+        Task EditClinic(Clinic clinic);
+        Task DeleteClinic(string name , string address);
+    }
+}
