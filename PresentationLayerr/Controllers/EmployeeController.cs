847c15698b80e021196bfb2b00fdcbfc67d283aa
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-10-20 22:33:30 +0300

    Add files via upload

---
+++ PresentationLayerr/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using BussinessLogic.DTOs;
+using BussinessLogic.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _service;
+
+        public EmployeeController(IEmployeeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<getEmployeeDto>> GetAllEmployee()
+        {
+           return Ok( await _service.GetAllEmployee());
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<AddEmployeeDto>> AddEmployee(AddEmployeeDto dto)
+        {
+            await _service.AddEmployee(dto);
+            return NoContent();
+        }
+    }
+}
