452e2b30c7366ed9a92d4370e56ac1f338edf6b3
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-10-22 19:54:45 +0300

    Add files via upload

---
+++ BussinessLogic/DTOs/AddReservationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic.DTOs
+{
+    public class AddReservationDto
+    {
+        public DateTime ReservationDate { get; set; }
+        public string DoctorName { get; set; }
+        public string PatientName { get; set; }
+    }
+}
---
+++ BussinessLogic/DTOs/GetReservationDto.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic.DTOs
+{
+    public class GetReservationDto
+    {
+        public DateTime ReservationDate { get; set; }
+        public string Status { get; set; }
+        public string DoctorName { get; set; }
+        public string PatientName { get; set; }
+    }
+}
