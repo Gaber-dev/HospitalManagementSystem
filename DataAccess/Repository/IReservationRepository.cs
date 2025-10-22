8fccd79dbd82c2daf3a1aaa6a99fbf6ed51573ad
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-10-23 00:37:48 +0300

    Add files via upload

---
+++ DataAccess/Repository/IReservationRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public interface IReservationRepository
+    {
+        Task<IEnumerable<Reservation>> GetAllReservation();
+        Task AddReservation(Reservation reservation);
+        Task<Reservation> GetReservationByDateandpname(DateTime time, string patientname);
+        Task UpdateReservation(Reservation updatedres);
+        Task DeleteReservation(DateTime date, string patientname);
+    }
+}
