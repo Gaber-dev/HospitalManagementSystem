83bd7ea0d3fffbe525da8d187c818c1fa27a6196
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-10-22 17:56:46 +0300

    Add files via upload

---
+++ BussinessLogic/DTOs/AddDoctorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic.DTOs
+{
+    public class AddDoctorDto
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string SpecialIn { get; set; }
+        public string ClinicName { get; set; }
+    }
+}
---
+++ BussinessLogic/DTOs/AddEmployeeDto.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic.DTOs
+{
+    public class AddEmployeeDto
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Phone { get; set; }
+        public string ClinicName { get; set; }
+    }
+}
