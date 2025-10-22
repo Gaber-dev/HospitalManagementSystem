f264c40ecd31e119d05e689609a5f1d822a9cdce
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-10-22 19:53:03 +0300

    Add files via upload

---
+++ PresentationLayerr/Controllers/ReservationController.cs
@@ -0,0 +1,31 @@
+﻿using BussinessLogic.DTOs;
+using BussinessLogic.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationService _service;
+        public ReservationController(IReservationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<GetReservationDto>> GetAllReservation()
+        {
+           return Ok(await _service.GetAllReservation());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddReservation(AddReservationDto dto)
+        {
+            await _service.AddReservation(dto);
+            return Ok();
+        }
+    }
+}
