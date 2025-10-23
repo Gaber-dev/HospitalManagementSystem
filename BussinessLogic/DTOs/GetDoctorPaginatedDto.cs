b7b459b908b520e37d416401bf26b43f5059ff5f
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-10-23 20:13:34 +0300

    Add files via upload

---
+++ BussinessLogic/DTOs/GetDoctorPaginatedDto.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic.DTOs
+{
+    public class GetDoctorPaginatedDto
+    {
+        public IEnumerable<GetDoctorDto> Items { get; set; }
+        public int Totalcount { get; set; }
+
+        public int pageNumber { get; set; }
+
+        public int pageSize { get; set; }
+    }
+}
